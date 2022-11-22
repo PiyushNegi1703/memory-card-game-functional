58b78196b3a0d327eb8a2a50e76cb7d99cc01230
PiyushNegi1703 <112609875+PiyushNegi1703@users.noreply.github.com>
2022-11-23 00:04:38 +0530

    Update login.js

--- login.js
+++ login.js
@@ -1,24 +1,27 @@
-var playButton = document.getElementById('play-button')
-const form = document.querySelector('form')
+const playButton = document.getElementById('play-button')
+const Nname = document.getElementById('name')
+const username = document.getElementById('username')
 
-function saveName(){
-    var Name = document.getElementById("name").value
-    var uName = document.getElementById("username").value
-    if(localStorage.getItem('name') == null){
-        localStorage.setItem('name', '[]');
-    }  
-    if(localStorage.getItem('username') == null){
-        localStorage.setItem('username', '[]');
+playButton.onclick = () => {
+    if (Nname.value == '' || username.value == '') {
+        alert('Please provide your Name and Username!')
     }
-    var old_Name = JSON.parse(localStorage.getItem('name'));
-    var old_uName = JSON.parse(localStorage.getItem('username'));
-    old_Name.push(Name);
-    old_uName.push(uName);
-    localStorage.setItem('name', JSON.stringify(old_Name))
-    localStorage.setItem('username', JSON.stringify(old_uName))
-}
+    else {
+        var Name = document.getElementById("name").value
+        var uName = document.getElementById("username").value
+        if (localStorage.getItem('name') == null) {
+            localStorage.setItem('name', '[]');
+        }
+        if (localStorage.getItem('username') == null) {
+            localStorage.setItem('username', '[]');
+        }
+        var old_Name = JSON.parse(localStorage.getItem('name'));
+        var old_uName = JSON.parse(localStorage.getItem('username'));
+        old_Name.push(Name);
+        old_uName.push(uName);
+        localStorage.setItem('name', JSON.stringify(old_Name))
+        localStorage.setItem('username', JSON.stringify(old_uName))
 
-playButton.addEventListener('click', () =>{
-    saveName()
-    location.href = './game.html'
-})
+        location.href = "./game.html"
+    }
+}
